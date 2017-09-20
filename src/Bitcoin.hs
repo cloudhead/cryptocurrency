92881b2212b9c1741fad3987bfae50158e9ed8e3
Alexis Sellier <alexis@cloudhead.io>
2017-09-21 00:16:40 +0200

    Rewrite MonadSTM code to use custom monads

--- src/Bitcoin.hs
+++ src/Bitcoin.hs
@@ -7,9 +7,7 @@ import           Crypto.Blockchain
 import qualified Crypto.Blockchain.Message as Message
 import           Crypto.Blockchain.Message (Message)
 
-import           Control.Concurrent.Classy (MonadConc)
-import           Control.Concurrent.Classy.Async (async)
-import           Control.Concurrent.Classy.STM
+import           Control.Concurrent.Async (async, concurrently_)
 import           Control.Concurrent (threadDelay)
 import           Control.Monad (forever)
 import           Control.Monad.Reader
@@ -26,37 +24,49 @@ io = liftIO
 
 processMessage
     :: ( MonadLogger m
-       , MonadIO m
-       , MonadSTM m
-       , MonadConc m )
-    => Env tx m
+       , MonadMempool tx m
+       , MonadBlock tx m
+       , MonadIO m )
+    => Env tx
     -> Message tx
     -> m ()
-processMessage Env { envMempool } (Message.Tx tx) = do
+processMessage env (Message.Tx tx) = do
     logInfoN "Tx"
-    modifyTVar envMempool (addTx tx)
-processMessage Env { envNewBlocks } (Message.Block blk) = do
+    writeMempool env [tx]
+processMessage env (Message.Block blk) = do
     logInfoN "Block"
-    putTMVar envNewBlocks blk
+    receiveNewBlock env blk
 processMessage _ Message.Ping =
     logInfoN "Ping"
 
 startNode
-    :: ( MonadReader (Env Tx' m) m
+    :: ( MonadReader (Env Tx') m
+       , MonadMempool Tx' m
+       , MonadBlock Tx' m
        , MonadLogger m
-       , MonadIO m
-       , MonadSTM m
-       , MonadConc m )
+       , IO ~ m )
     => NS.ServiceName
     -> [(NS.HostName, NS.ServiceName)]
     -> m ()
 startNode port peers = do
     net :: Internet (Message Tx') <- listen port
-    io . async $ connectToPeers net peers
-    io . async $ forever $ do
+    async $ connectToPeers net peers
+    async $ forever $ do
         broadcast net Message.Ping
         threadDelay $ 1000 * 1000
 
+    concurrently_ (forever mineBlock)
+                  (forever (listenForMessage net))
+
+listenForMessage
+    :: ( MonadReader (Env Tx') m
+       , MonadLogger m
+       , MonadMempool Tx' m
+       , MonadBlock Tx' m
+       , MonadIO m
+       , Socket net (Message Tx')
+       ) => net -> m ()
+listenForMessage net = do
     env@Env {..} <- ask
     forever $ do
         msg <- receive net
--- src/Crypto/Blockchain.hs
+++ src/Crypto/Blockchain.hs
@@ -15,15 +15,40 @@ import qualified Data.Sequence as Seq
 import           Data.Sequence   (Seq, (|>), (><))
 import           Data.Foldable (toList)
 import qualified Data.List.NonEmpty as NonEmpty
-import           Control.Monad (forever)
 import           Control.Monad.Reader
 import           Control.Monad.Logger
-import           Control.Monad.STM.Class (MonadSTM)
-import           Control.Monad.Conc.Class (MonadConc)
 import           Control.Monad.IO.Class (MonadIO)
-import           Control.Concurrent.Classy.STM
+import           Control.Concurrent.STM
+import           Control.Applicative (Alternative, (<|>))
 
 type Blockchain tx = Seq (Block tx)
+
+class Alternative m => MonadBlock tx m where
+    listenForBlock  :: Env tx -> m (Block tx)
+    receiveNewBlock :: Env tx -> Block tx -> m ()
+    proposeBlock    :: Env tx -> Block tx -> m ()
+    findBlock       :: Traversable t => Env tx -> t tx -> m (Block tx)
+
+instance MonadBlock tx STM where
+    listenForBlock Env { envNewBlocks } =
+        takeTMVar envNewBlocks
+    receiveNewBlock Env { envNewBlocks } blk =
+        putTMVar envNewBlocks blk
+    proposeBlock _ _ = undefined
+    findBlock _ _ = undefined
+
+class MonadMempool tx m where
+    readMempool   :: Env tx -> m (Seq tx)
+    writeMempool  :: Traversable t => Env tx -> t tx -> m ()
+    updateMempool :: Traversable t => Env tx -> t tx -> m ()
+
+instance MonadMempool tx STM where
+    readMempool Env { envMempool } =
+        fromMempool <$> readTVar envMempool
+    writeMempool Env { envMempool } txs =
+        modifyTVar envMempool (addTxs txs)
+    updateMempool Env { envMempool } txs =
+        undefined
 
 instance Validate (Blockchain a) where
     validate = validateBlockchain
@@ -77,15 +102,15 @@ addTxs :: Foldable t => t tx -> Mempool tx -> Mempool tx
 addTxs txs' (Mempool txs) =
     Mempool $ txs >< Seq.fromList (toList txs')
 
-data Env tx stm = Env
-    { envBlockchain :: TVar stm (Blockchain tx)
-    , envMempool    :: TVar stm (Mempool tx)
-    , envNewBlocks  :: TMVar stm (Block tx)
+data Env tx = Env
+    { envBlockchain :: TVar (Blockchain tx)
+    , envMempool    :: TVar (Mempool tx)
+    , envNewBlocks  :: TMVar (Block tx)
     , envLogger     :: Logger
     , envSeen       :: Set (Hashed (Message tx) SHA256)
     }
 
-newEnv :: MonadSTM stm => stm (Env tx stm)
+newEnv :: STM (Env tx)
 newEnv = do
     bc <- newTVar mempty
     mp <- newTVar mempty
@@ -101,31 +126,19 @@ newEnv = do
 messageIsNew :: Binary tx => Message tx -> Set (Hashed (Message tx) SHA256) -> Bool
 messageIsNew msg = not . Set.member (hashed msg)
 
-findBlock :: (MonadSTM m, Traversable t) => Env tx m -> t tx -> m (Block tx)
-findBlock = undefined
-
-listenForBlock :: MonadSTM m => Env tx m -> m (Block tx)
-listenForBlock Env { envNewBlocks } =
-    takeTMVar envNewBlocks
-
-proposeBlock :: MonadIO m => Block tx -> m ()
-proposeBlock = undefined
-
-updateMempool :: (MonadSTM m, Traversable t) => Env tx m -> t tx -> m ()
-updateMempool Env { envMempool } txs =
-    modifyTVar envMempool (addTxs txs)
-
-readTransactions :: MonadSTM m => Env tx m -> m (Seq tx)
-readTransactions Env { envMempool } =
-    fromMempool <$> readTVar envMempool
-
-mineBlocks :: (MonadSTM m, MonadConc m, MonadIO m, MonadReader (Env tx m) m, MonadLogger m) => m ()
-mineBlocks = forever $ do
+mineBlock
+    :: ( MonadIO m
+       , MonadMempool tx m
+       , MonadBlock tx m
+       , MonadReader (Env tx) m
+       , MonadLogger m)
+    => m ()
+mineBlock = do
     env <- ask
-    txs <- readTransactions env
-    result <- (Left <$> findBlock env txs) `orElse` (Right <$> listenForBlock env)
+    txs <- readMempool env
+    result <- Left <$> findBlock env txs <|> Right <$> listenForBlock env
     case result of
         Left foundBlock ->
-            proposeBlock foundBlock
+            proposeBlock env foundBlock
         Right receivedBlock ->
             updateMempool env (blockData receivedBlock)
